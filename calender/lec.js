c31b2ca90aafeaff347594a07a3d8458faad3012
pushgive19 <59918189+pushgive19@users.noreply.github.com>
2020-07-22 23:10:51 +0900

    none\

--- calender/lec.js
+++ calender/lec.js
@@ -1,6 +1,9 @@
+// ================================
+// START YOUR APP HERE
+// ================================
 const init = {
   monList: [
-    "january",
+    "January",
     "February",
     "March",
     "April",
@@ -40,43 +43,46 @@ const init = {
     d.setMonth(--this.monForChange);
     this.activeDate = d;
     return d;
-    // 왜 return 을 해줄까?
   },
-  addZero: (num) => (num < 10 ? `0${num}` : num),
-  activeDTage: null,
+  addZero: (num) => (num < 10 ? "0" + num : num),
+  activeDTag: null,
   getIndex: function (node) {
+    let index = 0;
     while ((node = node.previousElementSibling)) {
       index++;
     }
     return index;
   },
 };
-
+console.log(init, init.today, init["today"]);
 const $calBody = document.querySelector(".cal-body");
-const $btnNext = documnet.querySelector(".btn-cal.next");
-const $btnPrev = document.querySelector(".btn-call.prev");
+const $btnNext = document.querySelector(".btn-cal.next");
+const $btnPrev = document.querySelector(".btn-cal.prev");
 
+/**
+ * @param {number} date
+ * @param {number} dayIn
+ */
 function loadDate(date, dayIn) {
   document.querySelector(".cal-date").textContent = date;
   document.querySelector(".cal-day").textContent = init.dayList[dayIn];
 }
 
-loadDate(init.today.getDate(), init.today.getDay());
-
-function loadYYM(fullDate) {
-  let yy = fullDate.getFullMonth();
+/**
+ * @param {date} fullDate
+ */
+function loadYYMM(fullDate) {
+  let yy = fullDate.getFullYear();
   let mm = fullDate.getMonth();
   let firstDay = init.getFirstDay(yy, mm);
   let lastDay = init.getLastDay(yy, mm);
-  let markToday; /* for marking today date */
+  let markToday; // for marking today date
 
   if (mm === init.today.getMonth() && yy === init.today.getFullYear()) {
-    // 이게모지?
-    /*  */
     markToday = init.today.getDate();
   }
 
-  documnet.querySelector(".cal-month").textContent = init.monList[mm];
+  document.querySelector(".cal-month").textContent = init.monList[mm];
   document.querySelector(".cal-year").textContent = yy;
 
   let trtd = "";
@@ -91,16 +97,17 @@ function loadYYM(fullDate) {
       if (!startCount) {
         trtd += "<td>";
       } else {
-        let fullDate = `${yy}.${init.addZero(mm + 1)}.${init.addZero(
-          countDaty + 1
-        )}`;
+        let fullDate =
+          yy + "." + init.addZero(mm + 1) + "." + init.addZero(countDay + 1);
         trtd += '<td class="day';
-        trtd += markToday && markToday === countDay + 1 ? 'today" ' : '"';
+        trtd += markToday && markToday === countDay + 1 ? ' today" ' : '"';
+        //markToday 처음꺼는 왜줬을까?
+
         trtd += ` data-date="${countDay + 1}" data-fdate="${fullDate}">`;
       }
       trtd += startCount ? ++countDay : "";
       if (countDay === lastDay.getDate()) {
-        startCound = 0;
+        startCount = 0;
       }
       trtd += "</td>";
     }
@@ -108,29 +115,35 @@ function loadYYM(fullDate) {
   }
   $calBody.innerHTML = trtd;
 }
-loadYYMM(init.today);
 
+/**
+ * @param {string} val
+ */
 function createNewList(val) {
   let id = new Date().getTime() + "";
   let yy = init.activeDate.getFullYear();
   let mm = init.activeDate.getMonth() + 1;
   let dd = init.activeDate.getDate();
-
-  const $target = $calBody.querySelector(`.day[data-date"${dd}"]`);
+  ㄴ;
+  const $target = $calBody.querySelector(`.day[data-date="${dd}"]`);
 
   let date = yy + "." + init.addZero(mm) + "." + init.addZero(dd);
 
   let eventData = {};
   eventData["date"] = date;
+  console.log(eventData, eventData.date, eventData["date"]);
   eventData["memo"] = val;
-  eventDate["complete"] = false;
+  eventData["complete"] = false;
   eventData["id"] = id;
-  //왜 object를 이렇게 적을까? 왜 변수선언시 안에다 적지 않았을까?
   init.event.push(eventData);
   $todoList.appendChild(createLi(id, val, date));
 }
+
+loadYYMM(init.today);
+loadDate(init.today.getDate(), init.today.getDay());
+
 $btnNext.addEventListener("click", () => loadYYMM(init.nextMonth()));
-$btnPrev.addEventListener("clicK", () => loadYYMM(init.prevMonth()));
+$btnPrev.addEventListener("click", () => loadYYMM(init.prevMonth()));
 
 $calBody.addEventListener("click", (e) => {
   if (e.target.classList.contains("day")) {
@@ -142,6 +155,6 @@ $calBody.addEventListener("click", (e) => {
     e.target.classList.add("day-active");
     init.activeDTag = e.target;
     init.activeDate.setDate(day);
-    
+    reloadTodo();
   }
 });
