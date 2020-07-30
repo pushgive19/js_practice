86ab78ddfc9a771b27a492428f8c3b9b8393269a
pushgive19 <59918189+pushgive19@users.noreply.github.com>
2020-07-30 22:04:46 +0900

    calendar

--- calender/index.js
+++ calender/index.js
@@ -1,7 +1,16 @@
-const init = {
-  monList: [
-    "january",
-    "February",
+const initial = {
+  day: [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wendsday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ],
+  month: [
+    "January",
+    "Fabruary",
     "March",
     "April",
     "May",
@@ -13,135 +22,70 @@ const init = {
     "November",
     "December",
   ],
-  dayList: [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ],
   today: new Date(),
-  monForChange: new Date().getMonth(),
-  activeDate: new Date(),
+  addZero: (num) => (num < 10 ? `0${num}` : num),
+  thisMonth: function () {
+    return this.today.getMonth();
+  },
+  thisYear: function () {
+    return this.today.getFullYear();
+  },
   getFirstDay: (yy, mm) => new Date(yy, mm, 1),
   getLastDay: (yy, mm) => new Date(yy, mm + 1, 0),
-  nextMonth: function () {
-    let d = new Date();
-    d.setDate(1);
-    d.setMonth(++this.monForChange);
-    this.activeDate = d;
-    return d;
-  },
-  prevMonth: function () {
-    let d = new Date();
-    d.setDate(1);
-    d.setMonth(--this.monForChange);
-    this.activeDate = d;
-    return d;
-    // 왜 return 을 해줄까?
-  },
-  addZero: (num) => (num < 10 ? `0${num}` : num),
-  activeDTage: null,
-  getIndex: function (node) {
-    while ((node = node.previousElementSibling)) {
-      index++;
-    }
-    return index;
-  },
 };
+const todayDay = document.querySelector(".clicked__day"),
+  todayDate = document.querySelector(".clicked__date"),
+  ctrDay = document.querySelector(".ctr__box-day"),
+  ctrDate = document.querySelector(".ctr__box-date");
+let makeTable = document.querySelector(".calendar__table");
 
-const $calBody = document.querySelector(".cal-body");
-const $btnNext = documnet.querySelector(".btn-cal.next");
-const $btnPrev = document.querySelector(".btn-call.prev");
-
-function loadDate(date, dayIn) {
-  document.querySelector(".cal-date").textContent = date;
-  document.querySelector(".cal-day").textContent = init.dayList[dayIn];
+function printToday() {
+  const day = initial.today.getDay();
+  const date = initial.today.getDate();
+  todayDay.textContent = initial.day[day];
+  todayDate.textContent = date;
 }
-
-loadDate(init.today.getDate(), init.today.getDay());
-
-function loadYYM(fullDate) {
-  let yy = fullDate.getFullMonth();
-  let mm = fullDate.getMonth();
-  let firstDay = init.getFirstDay(yy, mm);
-  let lastDay = init.getLastDay(yy, mm);
-  let markToday; /* for marking today date */
-
-  if (mm === init.today.getMonth() && yy === init.today.getFullYear()) {
-    // 이게모지?
-    /*  */
-    markToday = init.today.getDate();
-  }
-
-  documnet.querySelector(".cal-month").textContent = init.monList[mm];
-  document.querySelector(".cal-year").textContent = yy;
-
-  let trtd = "";
-  let startCount;
-  let countDay = 0;
+function printCtr() {
+  ctrDay.textContent = initial.day[initial.today.getDay()];
+  ctrDate.textContent = initial.today.getDate();
+}
+function printCalendar() {
+  let saveDate = "";
+  let dayNum = 0;
+  const isFirst = initial
+    .getFirstDay(initial.thisYear(), initial.thisMonth())
+    .getDay();
+  const isLast = initial
+    .getLastDay(initial.thisYear(), initial.thisMonth())
+    .getDate();
   for (let i = 0; i < 6; i++) {
-    trtd += "<tr>";
+    saveDate += "<tr>";
     for (let j = 0; j < 7; j++) {
-      if (i === 0 && !startCount && j === firstDay.getDay()) {
-        startCount = 1;
-      }
-      if (!startCount) {
-        trtd += "<td>";
+      if (i === 0 && j < isFirst) {
+        saveDate += "<td></td>";
       } else {
-        let fullDate = `${yy}.${init.addZero(mm + 1)}.${init.addZero(
-          countDaty + 1
-        )}`;
-        trtd += '<td class="day';
-        trtd += markToday && markToday === countDay + 1 ? 'today" ' : '"';
-        trtd += ` data-date="${countDay + 1}" data-fdate="${fullDate}">`;
+        if (dayNum < isLast) {
+          // console.log(dayNum, isLast);
+          if (dayNum === initial.today.getDate() - 1) {
+            console.log(dayNum, dayNum - 1);
+            saveDate += `<td class="day today">${++dayNum}`;
+            saveDate += "</td>";
+          } else {
+            saveDate += `<td class="day">${++dayNum}`;
+            saveDate += "</td>";
+          }
+        }
       }
-      trtd += startCount ? ++countDay : "";
-      if (countDay === lastDay.getDate()) {
-        startCound = 0;
-      }
-      trtd += "</td>";
     }
-    trtd += "</tr>";
+    saveDate += "</tr>";
   }
-  $calBody.innerHTML = trtd;
+  makeTable.innerHTML += saveDate;
+  console.log(saveDate);
+  printCtr();
 }
-loadYYMM(init.today);
 
-function createNewList(val) {
-  let id = new Date().getTime() + "";
-  let yy = init.activeDate.getFullYear();
-  let mm = init.activeDate.getMonth() + 1;
-  let dd = init.activeDate.getDate();
-
-  const $target = $calBody.querySelector(`.day[data-date"${dd}"]`);
-
-  let date = yy + "." + init.addZero(mm) + "." + init.addZero(dd);
-
-  let eventData = {};
-  eventData["date"] = date;
-  eventData["memo"] = val;
-  eventDate["complete"] = false;
-  eventData["id"] = id;
-  //왜 object를 이렇게 적을까? 왜 변수선언시 안에다 적지 않았을까?
-  init.event.push(eventData);
-  $todoList.appendChild(createLi(id, val, date));
+function init() {
+  printToday();
+  printCalendar();
 }
-$btnNext.addEventListener("click", () => loadYYMM(init.nextMonth()));
-$btnPrev.addEventListener("clicK", () => loadYYMM(init.prevMonth()));
-
-$calBody.addEventListener("click", (e) => {
-  if (e.target.classList.contains("day")) {
-    if (init.activeDTag) {
-      init.activeDTag.classList.remove("day-active");
-    }
-    let day = Number(e.target.textContent);
-    loadDate(day, e.target.cellIndex);
-    e.target.classList.add("day-active");
-    init.activeDTag = e.target;
-    init.activeDate.setDate(day);
-    
-  }
-});
+init();
