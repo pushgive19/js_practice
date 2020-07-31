6fa6d6a9f02e09676b91e70e83c23cb0fb560ecf
pushgive19 <59918189+pushgive19@users.noreply.github.com>
2020-07-31 23:04:46 +0900

    button

--- calender/index.js
+++ calender/index.js
@@ -1,3 +1,4 @@
+// 문제1 button 그림자 남아있어
 const initial = {
   day: [
     "Sunday",
@@ -35,9 +36,13 @@ const initial = {
 };
 const todayDay = document.querySelector(".clicked__day"),
   todayDate = document.querySelector(".clicked__date"),
-  ctrDay = document.querySelector(".ctr__box-day"),
-  ctrDate = document.querySelector(".ctr__box-date");
+  ctrMonth = document.querySelector(".ctr__box-month"),
+  ctrYear = document.querySelector(".ctr__box-year"),
+  btnPrev = document.querySelector(".cal__btn.prev"),
+  btnNext = document.querySelector(".cal__btn.next");
 let makeTable = document.querySelector(".calendar__table");
+let clickMonth = initial.thisMonth();
+let clickYear = initial.thisYear();
 
 function printToday() {
   const day = initial.today.getDay();
@@ -45,19 +50,26 @@ function printToday() {
   todayDay.textContent = initial.day[day];
   todayDate.textContent = date;
 }
-function printCtr() {
-  ctrDay.textContent = initial.day[initial.today.getDay()];
-  ctrDate.textContent = initial.today.getDate();
+function printCtr(yy, mm) {
+  // let year = yy;
+  // let month = 0;
+  // if (mm === -1) {
+  //   year--;
+  //   month;
+  // }
+  ctrMonth.textContent = initial.month[mm];
+  ctrYear.textContent = yy;
+  console.log("yy,mm :>> ", yy, mm);
+  // ctrMonth.textContent = initial.month[initial.today.getDay()];
+  // ctrYear.textContent = initial.today.getFullYear();
 }
-function printCalendar() {
+function printCalendar(yy, mm) {
+  console.log("yy,mm :>> ", yy, mm);
+  printCtr(yy, mm);
   let saveDate = "";
   let dayNum = 0;
-  const isFirst = initial
-    .getFirstDay(initial.thisYear(), initial.thisMonth())
-    .getDay();
-  const isLast = initial
-    .getLastDay(initial.thisYear(), initial.thisMonth())
-    .getDate();
+  const isFirst = initial.getFirstDay(yy, mm).getDay();
+  const isLast = initial.getLastDay(yy, mm).getDate();
   for (let i = 0; i < 6; i++) {
     saveDate += "<tr>";
     for (let j = 0; j < 7; j++) {
@@ -66,8 +78,11 @@ function printCalendar() {
       } else {
         if (dayNum < isLast) {
           // console.log(dayNum, isLast);
-          if (dayNum === initial.today.getDate() - 1) {
-            console.log(dayNum, dayNum - 1);
+          if (
+            dayNum === initial.today.getDate() - 1 &&
+            mm === initial.thisMonth()
+          ) {
+            console.log(dayNum, dayNum - 1, "hi");
             saveDate += `<td class="day today">${++dayNum}`;
             saveDate += "</td>";
           } else {
@@ -79,13 +94,52 @@ function printCalendar() {
     }
     saveDate += "</tr>";
   }
-  makeTable.innerHTML += saveDate;
   console.log(saveDate);
-  printCtr();
+  makeTable.innerHTML += saveDate;
 }
-
+function handlePrev() {
+  // const prevMonth = new Date(yy, mm);
+  makeTable.innerHTML =
+    "<tr>" +
+    "<th>Sun</th>" +
+    "<th>Mon</th>" +
+    "<th>Tue</th>" +
+    "<th>Wen</th>" +
+    "<th>Thu</th>" +
+    "<th>Fri</th>" +
+    "<th>Sat</th>" +
+    "</tr>";
+  console.log("makeTable.innerHTML :>> ", makeTable.textContent);
+  clickMonth--;
+  if (clickMonth === -1) {
+    clickMonth = 11;
+    clickYear--;
+  }
+  printCalendar(clickYear, clickMonth);
+}
+function handleNext() {
+  makeTable.innerHTML =
+    "<tr>" +
+    "<th>Sun</th>" +
+    "<th>Mon</th>" +
+    "<th>Tue</th>" +
+    "<th>Wen</th>" +
+    "<th>Thu</th>" +
+    "<th>Fri</th>" +
+    "<th>Sat</th>" +
+    "</tr>";
+  console.log("makeTable.innerHTML :>> ", makeTable.textContent);
+  clickMonth++;
+  if (clickMonth === 12) {
+    clickMonth = 0;
+    clickYear++;
+  }
+  printCalendar(clickYear, clickMonth);
+}
 function init() {
   printToday();
-  printCalendar();
+  printCalendar(initial.thisYear(), initial.thisMonth());
+  btnPrev.addEventListener("click", handlePrev);
+  btnNext.addEventListener("click", handleNext);
 }
 init();
