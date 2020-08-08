e5e763ac8c5e8acb32cc6fd037abebd1c0aae20d
pushgive19 <59918189+pushgive19@users.noreply.github.com>
2020-08-08 23:51:24 +0900

    pr

--- dragNdrop/index.js
+++ dragNdrop/index.js
@@ -2,7 +2,8 @@ const dropzone = document.getElementById("dropzone");
 
 function onDragStart(event) {
   event.dataTransfer.setData("text/plain", event.currentTarget.dataset.table);
-  event.currentTArget.style.backgroundColor = "#e9c46a";
+  console.log("object :>> ", event.currentTarget.dataset.table);
+  event.currentTarget.style.backgroundColor = "#e9c46a";
 }
 function onDragEnd(event) {
   event.currentTarget.style.backgroundColor = "#2a9d8f";
@@ -11,17 +12,12 @@ function onDragOver(event) {
   event.preventDefault();
 }
 function onDrop(event) {
-  let id = evente.dataTransfer.getData("text");
+  let id = event.dataTransfer.getData("text");
 
   const draggableElement = document.getElementById(id);
   const clone = draggableElement.cloneNode(true);
   dropzone.appendChild(clone);
   event.dataTransfer.clearDate();
 }
-function init() {
-  onDragStart();
-  onDragEnd();
-  onDragOver();
-  onDrop();
-}
+function init() {}
 init();
